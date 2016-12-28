8edeebc9f6f171bf1348058ff680bc900b610592
unknown <benursal@hotmail.com>
2016-12-28 12:39:56 +0800

    woocommerce setup and other functions

--- functions.php
+++ functions.php
@@ -23,6 +23,77 @@ add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 // Additional Functions
 // =============================================================================
 
+function show_pre( $arr )
+{
+	echo '<pre>';
+	print_r( $arr );
+	echo '</pre>';
+}
+
+function woo_in_cart($product_id) {
+    global $woocommerce;
+ 
+    foreach($woocommerce->cart->get_cart() as $key => $val ) {
+        $_product = $val['data'];
+ 
+        if($product_id == $_product->id ) {
+            return true;
+        }
+    }
+ 
+    return false;
+}
+
 add_action( 'cornerstone_load_builder', function() {
 	echo '<style>.ps-container>.ps-scrollbar-y-rail>.ps-scrollbar-y {width: 10px !important;}</style>';
 });
+
+function default_no_quantities( $individually, $product ){
+	$individually = true;
+	return $individually;
+}
+
+add_filter( 'woocommerce_is_sold_individually', 'default_no_quantities', 10, 2 );
+
+
+// check for empty-cart get param to clear the cart
+
+add_action( 'init', 'woocommerce_clear_cart_url' );
+
+function woocommerce_clear_cart_url() {
+
+	global $woocommerce;
+
+	if ( isset( $_GET['empty-cart'] ) ) 
+	{
+		$woocommerce->cart->empty_cart();
+	}
+}
+
+
+function lime_add_to_cart_validation($passed, $product_id, $quantity, $variation_id = '', $variations= '') { 
+    global $woocommerce;
+	
+	// check if the item is already in the cart
+	if( woo_in_cart( $product_id ) )
+	{
+		$product = new WC_Product($product_id);
+		
+		wc_add_notice( __( 'You have already added <strong>' . $product->get_title() . '</strong>' , 'woocommerce' ), 'error' );
+		$passed = false;
+	}
+	
+	
+	// validate the number of items in cart if it does not exceed the allowed number
+	//$max_quantity = get_option('ywmmq_cart_maximum_quantity');
+	
+	if( count($woocommerce->cart->get_cart_item_quantities()) == get_option('ywmmq_cart_maximum_quantity') )
+	{
+		wc_add_notice( __( 'You already have completed the number of content items in your library' , 'woocommerce' ), 'error' );
+		$passed = false;
+	}
+	
+    return $passed;
+}
+
+add_action( 'woocommerce_add_to_cart_validation', 'lime_add_to_cart_validation', 10, 5 );
